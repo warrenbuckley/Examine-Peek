d39fc11300040233d3e002cff43a1264d0a00eb0
Warren Buckley <warren@hackmakedo.com>
2024-12-10 21:48:16 +0000

    ESLint rule to allow empty interfaces - needed for the Modal as it returns nothing

--- ExaminePeek/Client/eslint.config.js
+++ ExaminePeek/Client/eslint.config.js
@@ -5,9 +5,20 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {ignores: ["src/Api/*"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["src/Api/*"] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules:
+    {
+      "@typescript-eslint/no-empty-object-type": [
+        "error",
+        {
+          "allowInterfaces": "always"
+        }
+      ]
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
